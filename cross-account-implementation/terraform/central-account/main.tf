e72537e0e94108fd79965e9d4eff5baad8e44802
Srinivasu <srinivasular@unitedtechno.com>
2025-11-25 08:57:59 +0000

    issue fix

--- cross-account-implementation/terraform/central-account/main.tf
+++ cross-account-implementation/terraform/central-account/main.tf
@@ -212,14 +212,21 @@ resource "aws_iam_role_policy" "lambda_execution_policy" {
         Action = [
           "logs:CreateLogGroup",
           "logs:CreateLogStream",
-          "logs:PutLogEvents",
-          "logs:DescribeLogGroups",
-          "logs:DescribeLogStreams"
+          "logs:PutLogEvents"
         ]
         Resource = [
           "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/*",
           "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:log-group:/c7n/*"
         ]
+      },
+      {
+        Sid    = "CloudWatchLogsDescribe"
+        Effect = "Allow"
+        Action = [
+          "logs:DescribeLogGroups",
+          "logs:DescribeLogStreams"
+        ]
+        Resource = "*"
       },
       {
         Sid    = "SQSMailerQueue"
